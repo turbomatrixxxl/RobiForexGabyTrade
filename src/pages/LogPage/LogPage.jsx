451d896b1faf82b748f9c48c27b3031344f08d30
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2025-08-13 16:05:38 +0300

    finished logs page

--- src/pages/LogPage/LogPage.jsx
+++ src/pages/LogPage/LogPage.jsx
@@ -8,6 +8,9 @@ import styles from "./LogPage.module.css";
 
 export default function LogPage() {
   const theme = localStorage.getItem("theme") || "dark";
+  const cBots = localStorage.getItem("cBots")
+    ? JSON.parse(localStorage.getItem("cBots"))
+    : [];
 
   return (
     <div
@@ -20,6 +23,103 @@ export default function LogPage() {
           : styles.darkCont
       )}>
       <Grafic />
+      <div className={styles.tableArea} id="journalSection">
+        <table className={styles.table}>
+          <thead className={styles.tableHead}>
+            <tr className={styles.tableRow}>
+              <th
+                className={clsx(
+                  styles.tableHeaderCell,
+                  theme === "violet" && styles.thViolet
+                )}>
+                Time
+              </th>
+              <th
+                className={clsx(
+                  styles.tableHeaderCell,
+                  theme === "violet" && styles.thViolet
+                )}>
+                C-Bot
+              </th>
+              <th
+                className={clsx(
+                  styles.tableHeaderCell,
+                  theme === "violet" && styles.thViolet
+                )}>
+                Message
+              </th>
+            </tr>
+          </thead>
+          {cBots.lenght > 0 && (
+            <tbody className={styles.tableBody}>
+              {cBots.map((x, i) => (
+                <tr key={`cBot${i}`} className={styles.tableRow}>
+                  <td
+                    className={clsx(
+                      styles.tableCell,
+                      theme === "violet" && styles.thViolet
+                    )}>
+                    {x.time}
+                  </td>
+                  <td
+                    className={clsx(
+                      styles.tableCell,
+                      theme === "violet" && styles.thViolet,
+                      styles.cBotName
+                    )}>
+                    {x.cBotName}
+                  </td>
+                  <td
+                    className={clsx(
+                      styles.tableCell,
+                      theme === "violet" && styles.thViolet
+                    )}>
+                    {x.message}
+                  </td>
+                </tr>
+              ))}
+              {/* <tr className={styles.tableRow}>
+                <td
+                  className={clsx(
+                    theme === "violet" && styles.thViolet,
+                    styles.emptyMsg
+                  )}>
+                  27/07/2025 19:30
+                </td>
+                <td
+                  className={clsx(
+                    theme === "violet" && styles.thViolet,
+                    styles.emptyMsg,
+                    styles.cBotName
+                  )}>
+                  1
+                </td>
+                <td
+                  className={clsx(
+                    theme === "violet" && styles.thViolet,
+                    styles.emptyMsg
+                  )}>
+                  Trader started
+                </td>
+              </tr> */}
+            </tbody>
+          )}
+          {cBots.length === 0 && (
+            <tbody className={styles.tableBody}>
+              <tr className={styles.tableRow}>
+                <td
+                  colSpan={3}
+                  className={clsx(
+                    styles.emptyMsgNoLogs,
+                    theme === "violet" && styles.thViolet
+                  )}>
+                  No logs available
+                </td>
+              </tr>
+            </tbody>
+          )}
+        </table>
+      </div>
     </div>
   );
 }
